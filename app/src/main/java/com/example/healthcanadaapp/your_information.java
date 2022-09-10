b76cb0465213b174f5a8583b3fc1f5ac205da4d8
Asad <hasad514@gmail.com>
2022-09-10 18:32:39 -0400

    added your records screen (still have to finish implementing it)

---
+++ app/src/main/java/com/example/healthcanadaapp/your_information.java
@@ -0,0 +1,14 @@
+package com.example.healthcanadaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class your_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_information);
+    }
+}
