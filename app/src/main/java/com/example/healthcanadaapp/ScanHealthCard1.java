614b4c7b938f9b770c0e0d83360305925caa08b1
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2022-09-10 20:05:47 -0400

    Good changes

--- app/src/main/java/com/example/healthcanadaapp/ScanHealthCard1.java
+++ app/src/main/java/com/example/healthcanadaapp/ScanHealthCard1.java
@@ -1,14 +1,18 @@
 package com.example.healthcanadaapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -43,9 +47,20 @@ public class ScanHealthCard1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
+                startActivityForResult(intent,0);
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("result", "code:" + requestCode);
+        if (requestCode == 0) {
+            bar.setVisibility(View.VISIBLE);
+            startActivity(new Intent(this.getApplicationContext(), HomeScreen.class));
+        }
     }
 }
