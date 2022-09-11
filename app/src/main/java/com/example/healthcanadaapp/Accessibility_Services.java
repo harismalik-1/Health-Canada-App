7e12a6a23d27924f1c6b7721f4d50afd8b044e76
Asad <hasad514@gmail.com>
2022-09-11 00:22:01 -0400

    added Accessibility_Services page

---
+++ app/src/main/java/com/example/healthcanadaapp/Accessibility_Services.java
@@ -0,0 +1,14 @@
+package com.example.healthcanadaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accessibility_Services extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accessibility_services);
+    }
+}
