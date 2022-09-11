dc1af7a48107a12f9e6ff7fc95a3f51515c05240
Priyankdave1 <84639969+Priyankdave1@users.noreply.github.com>
2022-09-11 01:23:29 -0400

    Linked up everything

--- app/src/main/java/com/example/healthcanadaapp/HomeScreen.java
+++ app/src/main/java/com/example/healthcanadaapp/HomeScreen.java
@@ -12,6 +12,8 @@ public class HomeScreen extends AppCompatActivity {
 
     Button book;
     Button yourinfo;
+    Button profile;
+    Button accesibilty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +32,20 @@ public class HomeScreen extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),your_information.class));
+            }
+        });
+        profile = findViewById(R.id.profilebut);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Profile.class));
+            }
+        });
+        accesibilty = findViewById(R.id.acessibility_services_but);
+        accesibilty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Accessibility_Services.class));
             }
         });
     }
